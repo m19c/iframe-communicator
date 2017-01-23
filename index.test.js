91258790a6c8f0bdfd403875b049f2d0716d2f53
Marc Binder <marc.binder@boerse-go.de>
2017-01-23 13:21:07 +0100

    Add tests

---
+++ index.test.js
@@ -0,0 +1,33 @@
+import IFrameCommunicator from './';
+
+describe('IFrameCommunicator', () => {
+  describe('constructor()', () => {
+    it('it stores the given properties', () => {
+      const com = new IFrameCommunicator('jon', ['d', 'o', 'e']);
+      expect(com.id).toBe('jon');
+      expect(com.deps).toEqual(['d', 'o', 'e']);
+    });
+  });
+
+  describe('event-management', () => {
+    it('stores the listener', () => {
+      function onReady() {}
+
+      const com = new IFrameCommunicator('jon', ['doe']);
+      com.on('ready', onReady);
+
+      expect(com.listeners.ready).toBeDefined();
+      expect(com.listeners.ready[0]).toBe(onReady);
+    });
+
+    it('calls the registrated listener', () => {
+      const onReady = jest.fn();
+      const com = new IFrameCommunicator('jon', ['doe']);
+
+      com.on('ready', onReady);
+      com.fire('ready');
+
+      expect(onReady).toHaveBeenCalled();
+    });
+  });
+});
--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-import IFrameCommunicator from './';
-
-describe('IFrameCommunicator', () => {
-  it('works', () => {
-    const ic = new IFrameCommunicator('a', ['b', 'c']);
-  })
-});
